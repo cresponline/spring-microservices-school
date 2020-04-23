d2c31f84632b6913ddba0bf6745292f01b6bc99d
screspo <cresponline@gmail.com>
2020-04-23 14:03:37 +0200

    add relations in answer class

--- microservices-answers/src/main/java/com/screspo/microservice/app/answers/MicroservicesAnswersApplication.java
+++ microservices-answers/src/main/java/com/screspo/microservice/app/answers/MicroservicesAnswersApplication.java
@@ -2,10 +2,14 @@ package com.screspo.microservice.app.answers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.screspo.microservice.app.answers.models.entity",
+			 "com.screspo.microservices.app.commons.students.models.entity",
+			 "com.screspo.microservices.commons.exams.models.entity"})
 public class MicroservicesAnswersApplication {
 
 	public static void main(String[] args) {
--- microservices-answers/src/main/java/com/screspo/microservice/app/answers/models/entity/Answer.java
+++ microservices-answers/src/main/java/com/screspo/microservice/app/answers/models/entity/Answer.java
@@ -1,10 +1,16 @@
 package com.screspo.microservice.app.answers.models.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.screspo.microservices.app.commons.students.models.entity.Student;
+import com.screspo.microservices.commons.exams.models.entity.Question;
 
 import lombok.Data;
 
@@ -16,6 +22,13 @@ public class Answer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String text;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Student student;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	private Question question;
 
 }
